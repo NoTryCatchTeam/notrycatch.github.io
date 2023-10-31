3a51fc8a790c2e28c3a448764d0b18723df1a93e
swamp1820 <swamp1820@yandex.ru>
2023-10-31 22:10:07 +0300

    add header, footer, favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TeamComponent } from './team/team.component';
+import { CasesComponent } from './cases/cases.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactComponent } from './contacts/contact/contact.component';
+import { MemberComponent } from './team/member/member.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    TeamComponent,
+    CasesComponent,
+    AboutComponent,
+    ContactsComponent,
+    ContactComponent,
+    MemberComponent
   ],
   imports: [
     BrowserModule
