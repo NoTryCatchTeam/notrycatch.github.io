8e88d4937cfa5d4f1d7488f4902e13c9c828c307
swamp1820 <swamp1820@yandex.ru>
2023-11-02 21:13:40 +0300

    add fonts, avatars + refactoring

--- src/app/team/member/member.component.ts
+++ src/app/team/member/member.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-member',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./member.component.scss']
 })
 export class MemberComponent {
-
+  @Input() avatarName = '';
+  @Input() Name = '';
+  @Input() Position = '';
 }
