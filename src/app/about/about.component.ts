86de41f04721c3750a959eef87c35aeb42e99040
swamp1820 <swamp1820@yandex.ru>
2023-12-03 18:18:40 +0300

    about component init, small fixes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {  } from 'masonry-layout';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
 
+export class AboutComponent {
 }
